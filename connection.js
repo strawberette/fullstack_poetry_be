6ae0ac62fe496f4242bc2ae775a4d9bf604161b3
strawberette <noemi.scotellaro@gmail.com>
2022-01-27 13:37:01 +0000

    fix connection

--- connection.js
+++ connection.js
@@ -3,18 +3,17 @@ const { Sequelize } = require("sequelize");
 let connection;
 
 if (process.env.NODE_ENV === "PRODUCTION") {
-  module.exports.connection = new Sequelize(
-    `${process.env.DATABASE_URL}?sslmode=require`,
-    {
-      url: process.env.DATABASE_URI,
-      dialect: "postgres",
-      dialectOptions: {
-        ssl: {
-          rejectUnauthorized: false,
-        },
+  connection = new Sequelize(`${process.env.DATABASE_URL}?sslmode=require`, {
+    url: process.env.DATABASE_URI,
+    dialect: "postgres",
+    dialectOptions: {
+      ssl: {
+        rejectUnauthorized: false,
       },
-    }
-  );
+    },
+  });
 } else {
-  module.exports.connection = new Sequelize(process.env.DATABASE_URI);
+  connection = new Sequelize(process.env.DB_URI);
 }
+
+module.exports = connection;
