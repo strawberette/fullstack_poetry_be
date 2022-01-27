1f59ed09bba14f76fcddc0ed8713c298fe6683a6
strawberette <noemi.scotellaro@gmail.com>
2022-01-27 13:14:50 +0000

    add Heroku

--- connection.js
+++ connection.js
@@ -1,4 +1,20 @@
 const { Sequelize } = require("sequelize");
-const connection = new Sequelize(process.env.DB_URI);
 
-module.exports = connection;
+let connection;
+
+if (process.env.NODE_ENV === "PRODUCTION") {
+  module.exports.connection = new Sequelize(
+    `${process.env.DATABASE_URL}?sslmode=require`,
+    {
+      url: process.env.DATABASE_URI,
+      dialect: "postgres",
+      dialectOptions: {
+        ssl: {
+          rejectUnauthorized: false,
+        },
+      },
+    }
+  );
+} else {
+  module.exports.connection = new Sequelize(process.env.DATABASE_URI);
+}
