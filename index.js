cfc2795da16d32433aece541edc4b09eb3141e42
strawberette <noemi.scotellaro@gmail.com>
2022-01-31 00:29:34 +0000

    Add user management

--- index.js
+++ index.js
@@ -1,18 +1,33 @@
 require("dotenv").config();
 const express = require("express");
+const passport = require("passport");
 const connection = require("./connection");
 const Poem = require("./models/poem_structure");
 const cors = require("cors");
 const poemsRouter = require("./routes/poem.js");
+const User = require("./models/user");
+const userRouter = require("./routes/user");
+const {
+  registerStrategy,
+  loginStrategy,
+  verifyStrategy,
+} = require("./middleware/auth");
 
 const app = express();
 
 app.use(express.json());
 app.use(cors());
+app.use(passport.initialize());
 app.use("/poems", poemsRouter);
+app.use("/user", userRouter);
+
+passport.use("register", registerStrategy);
+passport.use("login", loginStrategy);
+passport.use(verifyStrategy);
 
 app.listen(process.env.PORT, () => {
   connection.authenticate();
   Poem.sync({ alter: true });
+  User.sync({ alter: true });
   console.log("App is online");
 });
