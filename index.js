022fa86818ba6432ec969f69b46a5328e94565d3
strawberette <noemi.scotellaro@gmail.com>
2022-02-01 13:02:03 +0000

    Revert "Initial commit"
    This reverts commit 34d77968cc6ff3a40b03a4bc2f59896bd285a7ea.

--- index.js
+++ index.js
@@ -2,7 +2,9 @@ require("dotenv").config();
 const express = require("express");
 const passport = require("passport");
 const connection = require("./connection");
+const Poem = require("./models/poem_structure");
 const cors = require("cors");
+const poemsRouter = require("./routes/poem.js");
 const User = require("./models/user");
 const userRouter = require("./routes/user");
 const {
@@ -16,6 +18,7 @@ const app = express();
 app.use(express.json());
 app.use(cors({ origin: "*" }));
 app.use(passport.initialize());
+app.use("/poems", poemsRouter);
 app.use("/user", userRouter);
 
 passport.use("register", registerStrategy);
@@ -24,6 +27,7 @@ passport.use(verifyStrategy);
 
 app.listen(process.env.PORT, () => {
   connection.authenticate();
+  Poem.sync({ alter: true });
   User.sync({ alter: true });
   console.log("App is online");
 });
