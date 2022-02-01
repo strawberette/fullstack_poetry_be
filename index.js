34d77968cc6ff3a40b03a4bc2f59896bd285a7ea
strawberette <noemi.scotellaro@gmail.com>
2022-02-01 12:58:44 +0000

    Initial commit

--- index.js
+++ index.js
@@ -2,9 +2,7 @@ require("dotenv").config();
 const express = require("express");
 const passport = require("passport");
 const connection = require("./connection");
-const Poem = require("./models/poem_structure");
 const cors = require("cors");
-const poemsRouter = require("./routes/poem.js");
 const User = require("./models/user");
 const userRouter = require("./routes/user");
 const {
@@ -18,7 +16,6 @@ const app = express();
 app.use(express.json());
 app.use(cors({ origin: "*" }));
 app.use(passport.initialize());
-app.use("/poems", poemsRouter);
 app.use("/user", userRouter);
 
 passport.use("register", registerStrategy);
@@ -27,7 +24,6 @@ passport.use(verifyStrategy);
 
 app.listen(process.env.PORT, () => {
   connection.authenticate();
-  Poem.sync({ alter: true });
   User.sync({ alter: true });
   console.log("App is online");
 });
